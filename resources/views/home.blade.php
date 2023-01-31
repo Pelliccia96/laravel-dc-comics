941b20b41b97d306cc548b78eb85b0c3ecf1b39f
Pelliccia96 <fra.pelliccia96@gmail.com>
2023-01-31 17:34:47 +0100

    Gg Wp + Bonus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,13 @@
 <div class="text-center my-5">
     <h1 class="text-white">{{ $title }}</h1>
 </div>
-<div class="py-5 text-center border rounded">
-    <a href="{{route("comics.index")}}"><button class="btn btn-primary">Go to Index</button></a>
-    <a href="{{route("comics.create")}}"><button class="btn btn-info mx-4">Go to Create</button></a>
+<div class="bg-secondary my-5 border rounded">
+    <div class="text-center py-5">
+        <h2 class="text-white fw-bold">HOME PAGE</h2>
+    </div>
+    <div class="py-5 mb-5 text-center">
+        <a href="{{route("comics.index")}}"><button class="btn btn-primary">Go to Index</button></a>
+        <a href="{{route("comics.create")}}"><button class="btn btn-info mx-4">Go to Create</button></a>
+    </div>
 </div>
 @endsection
